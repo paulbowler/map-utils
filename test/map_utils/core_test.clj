222e5ca028f8ec6e1e7abdd49a52cd5d85b130da
paulbowler <paul_bowler@yahoo.com>
2013-10-21 16:47:30 +0100

    Added commas for clarity

--- test/map_utils/core_test.clj
+++ test/map_utils/core_test.clj
@@ -2,23 +2,23 @@
   (:require [expectations :refer :all]
             [map-utils.core :refer :all]))
 
-(def m1 [{:name "Paul Bowler" :dob "01/01/1975"}
-         {:name "Bob Smith"   :dob "17/07/1971"}
-         {:name "Jim Brown"   :dob "20/02/1981"}
-         {:name "Kate Jones"  :dob "31/12/1985"}
-         {:name "Helen Pain"  :dob "09/10/1951"}])
+(def m1 [{:name "Paul Bowler", :dob "01/01/1975"}
+         {:name "Bob Smith",   :dob "17/07/1971"}
+         {:name "Jim Brown",   :dob "20/02/1981"}
+         {:name "Kate Jones",  :dob "31/12/1985"}
+         {:name "Helen Pain",  :dob "09/10/1951"}])
 
-(def m2 [{:name "Paul Bowler" :colour :red}
-         {:name "Bob Smith"   :colour :orange}
-         {:name "Jim Brown"   :colour :yellow}
-         {:name "Kate Jones"  :colour :green}
-         {:name "Helen Pain"  :colour :blue}])
+(def m2 [{:name "Paul Bowler", :colour :red}
+         {:name "Bob Smith",   :colour :orange}
+         {:name "Jim Brown",   :colour :yellow}
+         {:name "Kate Jones",  :colour :green}
+         {:name "Helen Pain",  :colour :blue}])
 
-(def m3 [{:name "Paul Bowler" :language :clojure}
-         {:name "Bob Smith"   :language :java}
-         {:name "Jim Brown"   :language :c++}
-         {:name "Kate Jones"  :language :Python}
-         {:name "Helen Pain"  :language :Ruby}])
+(def m3 [{:name "Paul Bowler", :language :clojure}
+         {:name "Bob Smith",   :language :java}
+         {:name "Jim Brown",   :language :c++}
+         {:name "Kate Jones",  :language :Python}
+         {:name "Helen Pain",  :language :Ruby}])
 
 (expect [{:language :clojure, :colour :red, :name "Paul Bowler", :dob "01/01/1975"}
          {:language :java, :colour :orange, :name "Bob Smith", :dob "17/07/1971"}
