4c273a3079d21aefee7504152a124dd18c4096aa
paulbowler <paul_bowler@yahoo.com>
2013-10-21 16:46:10 +0100

    Added test for key-join with expectations

--- test/map_utils/core_test.clj
+++ test/map_utils/core_test.clj
@@ -1,7 +1,29 @@
 (ns map-utils.core-test
-  (:require [clojure.test :refer :all]
+  (:require [expectations :refer :all]
             [map-utils.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def m1 [{:name "Paul Bowler" :dob "01/01/1975"}
+         {:name "Bob Smith"   :dob "17/07/1971"}
+         {:name "Jim Brown"   :dob "20/02/1981"}
+         {:name "Kate Jones"  :dob "31/12/1985"}
+         {:name "Helen Pain"  :dob "09/10/1951"}])
+
+(def m2 [{:name "Paul Bowler" :colour :red}
+         {:name "Bob Smith"   :colour :orange}
+         {:name "Jim Brown"   :colour :yellow}
+         {:name "Kate Jones"  :colour :green}
+         {:name "Helen Pain"  :colour :blue}])
+
+(def m3 [{:name "Paul Bowler" :language :clojure}
+         {:name "Bob Smith"   :language :java}
+         {:name "Jim Brown"   :language :c++}
+         {:name "Kate Jones"  :language :Python}
+         {:name "Helen Pain"  :language :Ruby}])
+
+(expect [{:language :clojure, :colour :red, :name "Paul Bowler", :dob "01/01/1975"}
+         {:language :java, :colour :orange, :name "Bob Smith", :dob "17/07/1971"}
+         {:language :c++, :colour :yellow, :name "Jim Brown", :dob "20/02/1981"}
+         {:language :Python, :colour :green, :name "Kate Jones", :dob "31/12/1985"}
+         {:language :Ruby, :colour :blue, :name "Helen Pain", :dob "09/10/1951"}]
+    (do
+      (key-join :name m1 m2 m3)))
